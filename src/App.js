48c11bba216631463b58fe138cd8f5a4a6748fbd
ejoor emmanuel <ejooremmanuel@gmail.com>
2022-01-19 16:03:20 +0100

    adding logs component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import EnhancedTable from "./components/Table/Table";
 import Dashboard from "./screen/dashboard/dashboard";
 import Prebook from "./screen/prebook/prebook";
 
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path="/admin/dashboard" element={<Dashboard />} />
         <Route exact path="/admin/prebook" element={<Prebook />} />
+        <Route exact path="/admin/logs" element={<EnhancedTable />} />
       </Routes>
     </Router>
   );
