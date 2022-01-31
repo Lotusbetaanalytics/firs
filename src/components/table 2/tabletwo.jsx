72c1e6922ff78c9c7a934328463f9c409123c823
ejoor emmanuel <ejooremmanuel@gmail.com>
2022-01-31 15:41:36 +0100

    fixing log component

--- src/components/table 2/tabletwo.jsx
+++ src/components/table 2/tabletwo.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MaterialTable from "material-table";
 import { useDispatch, useSelector } from "react-redux";
 import { logAction } from "../../redux/actions_/logActions";
 
 export const Tabletwo = () => {
   const dispatch = useDispatch();
-
+  const [tableData, setTableData] = useState([]);
   const getlog = useSelector((state) => state.getlog);
-  const { data } = getlog;
+
+  useEffect(() => {
+    if (getlog.success) {
+      setTableData(getlog.data);
+    }
+  }, [getlog, tableData]);
 
   useEffect(() => {
     dispatch(logAction());
@@ -16,65 +21,69 @@ export const Tabletwo = () => {
   return (
     <div>
       <div style={{ maxWidth: "100%" }}>
-        <MaterialTable
-          columns={[
-            { title: "Name", field: "name" },
-            { title: "Email", field: "email" },
-            {
-              title: "Phone Number",
-              field: "phoneNumber",
-            },
-            {
-              title: "Company Name",
-              field: "company",
-            },
-            {
-              title: "Laptop",
-              field: "laptop",
-            },
-            {
-              title: "Host Name",
-              field: "host.firstName",
-            },
-            {
-              title: "Time",
-              field: "time",
-            },
-            {
-              title: "Date",
-              field: "date",
-            },
-            {
-              title: "Purpose",
-              field: "purpose",
-            },
-            {
-              title: "Time in",
-              field: "timeIn",
-            },
-            {
-              title: "Time out",
-              field: "timeOut",
-            },
-            {
-              title: "Token",
-              field: "token",
-            },
-            {
-              title: "Active",
-              field: "isActive",
-            },
-            {
-              title: "Status",
-              field: "status",
-            },
-          ]}
-          data={data}
-          title="Visitors Log"
-          options={{
-            exportButton: true,
-          }}
-        />
+        {getlog.loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <MaterialTable
+            columns={[
+              { title: "Name", field: "name" },
+              { title: "Email", field: "email" },
+              {
+                title: "Phone Number",
+                field: "phoneNumber",
+              },
+              {
+                title: "Company Name",
+                field: "company",
+              },
+              {
+                title: "Laptop",
+                field: "laptop",
+              },
+              {
+                title: "Host Name",
+                field: "host.firstName",
+              },
+              {
+                title: "Time",
+                field: "time",
+              },
+              {
+                title: "Date",
+                field: "date",
+              },
+              {
+                title: "Purpose",
+                field: "purpose",
+              },
+              {
+                title: "Time in",
+                field: "timeIn",
+              },
+              {
+                title: "Time out",
+                field: "timeOut",
+              },
+              {
+                title: "Token",
+                field: "token",
+              },
+              {
+                title: "Active",
+                field: "isActive",
+              },
+              {
+                title: "Status",
+                field: "status",
+              },
+            ]}
+            data={tableData}
+            title="Visitors Log"
+            options={{
+              exportButton: true,
+            }}
+          />
+        )}
       </div>
     </div>
   );
